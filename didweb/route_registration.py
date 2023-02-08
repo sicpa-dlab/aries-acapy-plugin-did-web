55f11dab3924ed3dbc1cdde96d038a703a45a4bf
Clément Humbert <clement.humbert@sicpa.com>
2023-02-08 16:21:23 +0100

    fix: route public did using the context route manager

--- didweb/route_registration.py
+++ didweb/route_registration.py
@@ -1,4 +1,8 @@
-from aries_cloudagent.protocols.routing.v1_0.manager import RoutingManager
+from profile import Profile
+
+from aries_cloudagent.protocols.coordinate_mediation.v1_0.route_manager import (
+    RouteManager,
+)
 from aries_cloudagent.protocols.routing.v1_0.models.route_record import RouteRecord
 from aries_cloudagent.wallet.base import BaseWallet
 
@@ -6,11 +10,14 @@ from aries_cloudagent.wallet.base import BaseWallet
 class RouteRegistrar:
     """No route ? No worries."""
 
-    def __init__(self, wallet: BaseWallet, routing_manager: RoutingManager):
+    def __init__(
+        self, profile: Profile, wallet: BaseWallet, routing_manager: RouteManager
+    ):
+        self.__profile = profile
         self.__wallet = wallet
         self.__routing_manager = routing_manager
 
-    async def register_route(self, did: str, wallet_id: str) -> RouteRecord:
+    async def register_route(self, did: str) -> RouteRecord:
         """
         Register a route given a did and a recipient wallet
         :param did: DID on the receiving end of the route
@@ -18,6 +25,6 @@ class RouteRegistrar:
         :return:
         """
         did_record = await self.__wallet.get_local_did(did)
-        return await self.__routing_manager.create_route_record(
-            recipient_key=did_record.verkey, internal_wallet_id=wallet_id
+        return await self.__routing_manager.route_public_did(
+            profile=self.__profile, verkey=did_record.verkey
         )
--- didweb/routes/register_route.py
+++ didweb/routes/register_route.py
@@ -2,8 +2,12 @@ from aiohttp import web
 from aiohttp_apispec import querystring_schema, response_schema
 from aiohttp_apispec.decorators import docs
 from aries_cloudagent.admin.request_context import AdminRequestContext
-from aries_cloudagent.protocols.routing.v1_0.manager import RoutingManager
-from aries_cloudagent.protocols.routing.v1_0.models.route_record import RouteRecordSchema
+from aries_cloudagent.protocols.coordinate_mediation.v1_0.messages.keylist_update import (
+    KeylistUpdateSchema,
+)
+from aries_cloudagent.protocols.coordinate_mediation.v1_0.route_manager import (
+    RouteManager,
+)
 from aries_cloudagent.wallet.base import BaseWallet
 
 from ..route_registration import RouteRegistrar
@@ -16,7 +20,7 @@ from .schemas import DIDSchema
     summary="Registers a route for a given DID within a multi-tenanted agent.",
 )
 @querystring_schema(DIDSchema())
-@response_schema(RouteRecordSchema())
+@response_schema(KeylistUpdateSchema())
 async def register_route(request: web.Request):
     did = request.query.get("did")
     if not did:
@@ -24,21 +28,14 @@ async def register_route(request: web.Request):
 
     context: AdminRequestContext = request["context"]
 
-    print(context.profile.settings)
-    try:
-        wallet_id = context.profile.settings["wallet.id"]
-    except KeyError:
-        return web.Response(
-            status=404,
-            text="Registering routes is not necessary for single-tenant agents.",
-        )
-
     async with context.profile.transaction() as transaction:
         route_registrar = RouteRegistrar(
-            transaction.inject(BaseWallet), RoutingManager(context.profile)
+            context.profile,
+            transaction.inject(BaseWallet),
+            transaction.inject(RouteManager),
         )
-        route_record = await route_registrar.register_route(did, wallet_id)
+        keylist_update = await route_registrar.register_route(did)
 
         await transaction.commit()
 
-    return web.Response(text=RouteRecordSchema().dumps(route_record))
+    return web.Response(text=KeylistUpdateSchema().dumps(keylist_update))
