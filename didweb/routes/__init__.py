f7383cc72bfe7f685e21994939be75fd53ad8a1d
Nemanja Patrnogic <nemanja.patrnogic@sicpa.com>
2023-02-27 11:16:48 +0100

    fix: remove HEAD request method for did doc
    Signed-off-by: Nemanja Patrnogic <nemanja.patrnogic@sicpa.com>

--- didweb/routes/__init__.py
+++ didweb/routes/__init__.py
@@ -1,18 +1,17 @@
 from aiohttp import web
 
-from .mark_did_public import set_public_did
-from .register_route import register_route
 from .get_diddoc import fetch_diddoc
-from .rotate_key import rotate_key
-
+from .mark_did_public import set_public_did
 from .openapi_config import SPEC_URI, OPENAPI_TAG
+from .register_route import register_route
+from .rotate_key import rotate_key
 
 
 async def register(app: web.Application):
     """Register routes."""
     app.add_routes(
         [
-            web.get("/didweb/diddoc", fetch_diddoc),
+            web.get("/didweb/diddoc", fetch_diddoc, allow_head=False),
             web.put("/didweb/rotate", rotate_key),
             web.put("/routing/register_route", register_route),
             web.put("/routing/set_public_did", set_public_did),
